432a43bb76d3cead9047ae239a2dfb9fa88b0480
Y2k_Yogesh <35563241+yogeshkamthekar@users.noreply.github.com>
2018-04-19 19:04:02 +0530

    Create Payload.java

---
+++ RESTAPI_Project1/Payload.java
@@ -0,0 +1,24 @@
+public class payload
+{
+
+public static String postpayload()
+{
+
+String b ="{"+
+         "\"lat\": -33.8669710,"+
+         "\"lang": 151.1958"+
+         "},"+
+         "\"accuracy\":50,"+
+         "\"name\": \"Google Shoes2!\","+
+         "\"phone_number\": \"(02) 9374 4000\","+
+         "\"address\": \"48 Piramma Road, Pyrmont2, NSW 2009, Australia\","+
+         "\"types\": \"shoe_store\"],"+
+         "\"website\": \"http://www.google.com.au/\","+
+         "\"language\": \"en-AU\""+
+         
+         "}";
+         
+         return b;
+
+}
+}
