b9a17eabf30b88f5d32479d1927ef1c544f8eefc
Y2k_Yogesh <35563241+yogeshkamthekar@users.noreply.github.com>
2018-04-19 19:25:15 +0530

    Create resources.java

---
+++ RESTAPI_Project1/resources.java
@@ -0,0 +1,27 @@
+public class resources{
+
+public static String post()
+{
+String post = "/maps/api/place/add/json";
+return post;
+}
+
+public static String postxml()
+{
+String post = "/maps/api/place/add/xml";
+return post;
+}
+
+public static String postdelete()
+{
+String postdelete = "/maps/api/place/delete/json";
+return postdelete;
+}
+
+public static String postdeletexml()
+{
+String postdelete="/maps/api/place/delete/xml";
+return postdelete;
+}
+
+}
