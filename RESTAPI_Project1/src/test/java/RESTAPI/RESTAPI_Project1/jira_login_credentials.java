dfb475ca4186289077a507362bd628c2036b68f9
Y2k_Yogesh <35563241+yogeshkamthekar@users.noreply.github.com>
2018-05-01 08:52:46 +0000

    Update jira_login_credentials.java

--- RESTAPI_Project1/src/test/java/RESTAPI/RESTAPI_Project1/jira_login_credentials.java
+++ RESTAPI_Project1/src/test/java/RESTAPI/RESTAPI_Project1/jira_login_credentials.java
@@ -1,32 +1,39 @@
 package RESTAPI.RESTAPI_Project1;
 
-//import org.junit.Test;
-//import org.junit.Before;
-/*import io.restassured.RestAssured.*;
-import org.json.simple.JSONObject;
-import org.testng.annotations.Test;
-
-import io.restassured.response.Response;
 import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;*/
-import org.testng.annotations.Test;
-
-import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
-//import static com.jayway.restassured.RestAssured.expect;
 import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.*;
+import java.util.Properties;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+　
+　
 public class jira_login_credentials 
     
 {
     
-
+    static Response res;
     @Test
 	public static void postwithcredentials()
 	{
         RestAssured.baseURI="http://localhost:8085/rest";
-        Response response1 = given().
+         
+        res=given().
 		contentType("application/json").
 		body("{\"usename\":\"administrator\",\"password\":\"test\"}").
 		when().
